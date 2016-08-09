d6585e8800297736a688d5d2252019c34b789786
Erik <erik.duarte@focusnetworks.com.br>
2016-08-08 23:06:52 -0300

    Adding passport initial configs and play with modifiers, virtual attributes, validation and index creation on User Model

--- README.md
+++ README.md
@@ -9,6 +9,9 @@ Start the project server with **npm start** or **node server**;
 
 ### Project Timeline
 
+:calendar: **08-08-2016** -> Working with some modifiers, virtual attributes, validations and index creation
+on User model. Also, adding initial configuration of passport middleware for local strategy auth.
+
 :calendar: **07-08-2016** -> Configure mongoose and create CRUD on backend for User Schema/Model.
 
 :calendar: **05-08-2016** -> Add EJS template engine and configure express-session.
--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -4,9 +4,58 @@ var mongoose = require('mongoose'),
 var UserSchema = new Schema({
     firstName : String,
     lastName : String,
-    email : String,
-    userName : String,
-    password : String
+    email : {
+      type : String,
+      index : true,
+      match : /.+\@.+\..+/
+    },
+    website : {
+      type : String,
+      get : function(url) {
+        if(!url) {
+          return url;
+        } else {
+          if(url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+            url = 'http://' + url;
+          }
+
+          return url;
+        }
+      }
+    },
+    userName : {
+      type : String,
+      trim : true,
+      unique : true,
+      required : true
+    },
+    password : {
+      type : String,
+      validate : [
+        function(password) {
+          return password.length >= 6;
+        },
+        'Password should be longer!'
+      ]
+    },
+    created : {
+      type : Date,
+      default: Date.now
+    },
+    role : {
+      type : String,
+      enum : ['Admin', 'Owner', 'User']
+    }
 });
+
+UserSchema.virtual('fullname').get(function() {
+  return this.firstName + ' ' + this.lastName;
+}).set(function() {
+  var splitName = fullname.split(' ');
+  this.firstName = splitName[0] || '';
+  this.lastName = splitName[1] || '';
+});
+
+UserSchema.set('toJSON', { getters: true, virtuals: true });
 
 mongoose.model('User', UserSchema);
